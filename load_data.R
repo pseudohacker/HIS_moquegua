d410042fd4cae528665ce03743b9deb486f01ff4
Javier Loza <pseudohacker@gmail.com>
2025-04-29 15:29:05 -0500

    unrar

--- load_data.R
+++ load_data.R
@@ -1,11 +1,29 @@
+# Paqutes ----
+
 pacman::p_load(dplyr,
                purrr)
+
+
+# Load ----
 
 # query <- "https://files.jlh.work/csvs.rar"
 # url_encoded <- utils::URLencode(query, reserved = F)
 # url <- url_encoded
 # destfile <- here::here("input",glue::glue("csvs.rar"))
 # curl::curl_download(url, destfile)
+
+## Rutas ----
+# remote_route <- "s3://files.jlh.work/credicorp/"
+local_route <- here::here("input")
+
+
+## descargas ----
+file <- here::here(local_route,"csvs.rar")
+cmd_str <- glue::glue("unrar x {file} {local_route}/")
+cat(cmd_str)
+system(cmd_str)
+
+
 #
 # query <- "https://files.jlh.work/HIS318.DBF"
 # url_encoded <- utils::URLencode(query, reserved = F)
@@ -20,7 +38,7 @@ pacman::p_load(dplyr,
 # curl::curl_download(url, destfile)
 
 data_path <- "input/csvs"
-files <- dir(here::here(data_path), pattern = "^NOMINAL.*GENERAL_202") # get file names
+files <- dir(here::here(data_path), pattern = "^NOMINAL.*GENERAL_2023") # get file names
 dt <- files %>%
   # read in all the files, appending the path before the filename
   map(~ rio::import(file.path(data_path, .))) %>%
